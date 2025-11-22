df32eb22574897740637f4c890a2df229c859313
A439 <18359153400@163.com>
2025-11-22 13:15:20 +0800

    Update get_list.py

--- get_list.py
+++ get_list.py
@@ -4,16 +4,16 @@ import json
 from pathlib import Path
 
 
-def sha256(file_path, chunk_size=8192):
-    """SHA-256"""
-    sha256_hash = hashlib.sha256()
+def calculate_md5(file_path):
+    """计算文件的MD5值"""
+    hash_md5 = hashlib.md5()
     try:
         with open(file_path, "rb") as f:
-            for chunk in iter(lambda: f.read(chunk_size), b""):
-                sha256_hash.update(chunk)
-        return sha256_hash.hexdigest()
-    except IOError:
-        print(f"文件 {file_path} 打开失败！")
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+    except Exception as e:
+        print(f"错误：无法读取文件 {file_path} - {e}")
         return None
 
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
                 if not file_name.startswith("."):
                     file_path = os.path.join(root, file_name)
                     print(f"正在处理文件 {file_path}...")
-                    md5_value = sha256(file_path)
+                    md5_value = calculate_md5(file_path)
                     if md5_value:
                         files_list[os.path.relpath(file_path, dir_name)] = md5_value
 
